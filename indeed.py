1fcd69be04f5101c4d82d1d0ae5e0afceb4ac64f
dotoryeee <bayer@kakao.com>
2020-12-06 21:38:13 +0900

    get company name

--- indeed.py
+++ indeed.py
@@ -28,5 +28,7 @@ def extract_indeed_jobs(last_page):
     results = soup.find_all('div',{'class' : 'jobsearch-SerpJobCard'})
     for result in results:
         title = result.find('h2', {'class': 'title'}).find('a')['title']
-        print(title)
+        company = result.find('span',{'class': 'company'}).string.strip()
+        print(f'{title} / {company}')
+    
     return jobs
