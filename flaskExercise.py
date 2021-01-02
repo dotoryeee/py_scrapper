230b6b6a486bfef87688b2ee36385308118de946
dotoryeee <bayer@kakao.com>
2021-01-02 23:34:14 +0900

    first html

--- flaskExercise.py
+++ flaskExercise.py
@@ -1,13 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask('superScraper')
 
 @app.route("/")
 def home():
-    return 'hello'
-
-@app.route('/contact')
-def potato():
-    return 'contact page'
+    return render_template("potato.html")
 
 app.run(host = 'localhost')
