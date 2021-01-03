685f9b9c66a79b121ad016e467df0e0fc3b15574
dotoryeee <bayer@kakao.com>
2021-01-03 19:08:54 +0900

    apply scraper

--- flaskExercise.py
+++ flaskExercise.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-
+from scrapper import scrapJobs
 app = Flask('superScraper')
 
 @app.route("/")
@@ -9,6 +9,12 @@ def home():
 @app.route('/report')
 def report():
     word = request.args.get('word')
+    if word:
+        word = word.lower()
+        jobs = scrapJobs(word)
+        print(jobs)
+    else:
+        return redirect("/")
     return render_template('report.html', searching_by=word)
 
 app.run(host = 'localhost')
