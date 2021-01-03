51456c994ec98ec0afa69e0ff9cb9a2ce9f2af98
dotoryeee <bayer@kakao.com>
2021-01-03 14:14:27 +0900

    flask rendering

--- flaskExercise.py
+++ flaskExercise.py
@@ -1,9 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask('superScraper')
 
 @app.route("/")
 def home():
     return render_template("potato.html")
+
+@app.route('/report')
+def report():
+    word = request.args.get('word')
+    return render_template('report.html', searching_by=word)
 
 app.run(host = 'localhost')
