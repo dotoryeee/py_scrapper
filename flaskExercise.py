7b21f78422eb02278c60cf79de9d796752e97846
dotoryeee <bayer@kakao.com>
2021-01-05 01:15:42 +0900

    spread jobs

--- flaskExercise.py
+++ flaskExercise.py
@@ -13,9 +13,9 @@ def report():
     word = request.args.get('word')
     if word:
         word = word.lower()
-        fromDb = db.get(word)
-        if fromDb:
-            jobs = fromDb
+        existingJobs = db.get(word)
+        if existingJobs:
+            jobs = existingJobs
         else:
             jobs = scrapJobs(word)
             db[word] = jobs
@@ -23,6 +23,7 @@ def report():
         return redirect("/")
     return render_template('report.html',
                            resultNumber=len(jobs),
-                           searching_by=word)
+                           searching_by=word,
+                           jobs=jobs)
 
 app.run(host = 'localhost')
