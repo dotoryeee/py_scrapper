e23bcfa2d87e44c05d6df769c56461d3c22faf8c
dotoryeee <bayer@kakao.com>
2021-02-04 18:55:00 +0900

    complete read page

--- challenge/210204.py
+++ challenge/210204.py
@@ -1,5 +1,7 @@
 import requests
 from flask import Flask, render_template, request
+from bs4 import BeautifulSoup
+import json
 
 """
 When you try to scrape reddit make sure to send the 'headers' on your request.
@@ -8,8 +10,8 @@ that we are a normal computer and not a python script.
 How to use: requests.get(url, headers=headers)
 """
 
-headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'}
-
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.106 Whale/2.8.108.15 Safari/537.36'}
 
 """
 All subreddits have the same url:
@@ -32,8 +34,68 @@ subreddits = [
     "django"
 ]
 
+url = 'https://www.reddit.com/r/javascript'
+
+
+def conn(url):
+    print(f'connecting to {url}')
+    r = requests.get(url, headers=headers)
+    return (r.text)
+
+
+def extractDict(data):
+    print('extracting html')
+    soup = BeautifulSoup(data, 'html.parser')
+    soup = soup.find('body')
+    soup = soup.find('script', id='data').string
+    return soup[14:len(soup) - 1]  # 앞 뒤로 글자 자르기
+
+
+def dataToDict(data):
+    print('extracting dict')
+    data = json.loads(data)
+    return data
+
+def getParameter():
+    try:
+        parameter = request.args.get('read')
+        return parameter
+    except:
+        return None
+
+
+# DAO
+# conn -> extractDict -> extractData
+def MakeInfo(url):
+    data = conn(url)
+    data = extractDict(data)
+    data = dataToDict(data)
+    data = data['posts']['models']
+    datas = []
+    print('building Information')
+    for line in data.values():
+        title = line['title']
+        score = line['score']
+        url = line['permalink']
+        data = [title, score, url]
+        datas.append(data)
+    print('start rendering')
+    return datas
 
 app = Flask("DayEleven")
 
 
-app.run(host="0.0.0.0")
+@app.route('/')
+def home():
+    return render_template('home.html',list=subreddits)
+
+
+@app.route('/read')
+def board():
+    param = getParameter()
+    print(f'access board page({param})')
+    data = MakeInfo(url)
+    return render_template('read.html', data=data)
+
+
+app.run(host='localhost')
