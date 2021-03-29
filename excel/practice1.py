d109d55eff8ea36e40c4c510ad2df3fed248cad8
dotoryeee <bayer@kakao.com>
2021-03-29 23:35:09 +0900

    excel

--- excel/practice1.py
+++ excel/practice1.py
@@ -20,10 +20,10 @@ print(sheetCursor["A1"])
 print(sheetCursor["A1"].value)  # 셀 값 출력
 print(sheetCursor.cell(row=1, column=1).value)  # == A1
 print(sheetCursor["A2"].value)  # 값이 없으면 None 출력
-print("--------------------------------------")
+print("---------------------------------------------------------")
 sheetCursor.cell(column=2, row=1, value="yeah")  # == B1
 print(sheetCursor["B1"].value)
-print("--------------------------------------")
+print("---------------------------------------------------------")
 # 5 X 5 랜덤값 채우기
 for x in range(1, 6):
     for y in range(1, 6):
@@ -46,14 +46,14 @@ for i in range(1, 51):
 
 col_B = sheetCursor["B"]
 print(col_B)
-print("--------------------------------------")
+print("---------------------------------------------------------")
 
 
 for cell in col_B:
     print(cell.value, end=" ")
 
 print()
-print("--------------------------------------")
+print("---------------------------------------------------------")
 
 col_range = sheetCursor["B:C"]
 for cols in col_range:
@@ -61,7 +61,7 @@ for cols in col_range:
         print(cell.value, end=" ")
     print()
 
-print("--------------------------------------")
+print("---------------------------------------------------------")
 print(wb.sheetnames)
 
 # 표에서 제목을 제외한 데이터만 가져오기
@@ -73,7 +73,7 @@ for rows in row_range:
     print()
 
 print()
-print("--------------------------------------")
+print("---------------------------------------------------------")
 for rows in row_range:
     for cell in rows:
         xy = coordinate_from_string(cell.coordinate)
@@ -82,10 +82,18 @@ for rows in row_range:
 
 
 print()
-print("--------------------------------------")
+print("---------------------------------------------------------")
 
 for row in sheetCursor.iter_rows():
     print(row[1].value)  # B열의 모든 값 출력
+
+print()
+print("---------------------------------------------------------")
+# Range 제한 최종
+for row in sheetCursor.iter_rows(min_row=3, max_row=9, min_col=1, max_col=3):
+    for cell in row:
+        print(cell.value, end="\t")
+    print("")
 
 wb.save("test.xlsx")
 wb.close()
--- excel/practice2.py
+++ excel/practice2.py
@@ -6,3 +6,42 @@ ws = wb.active
 for x in range(1, ws.max_row+1):
     for y in range(1, ws.max_column+1):
         print(ws.cell(row=x, column=y).value, end=" ")
+
+ws = wb["test2"]
+
+print()
+print("---------------------------------------------------------")
+
+# test2 시트의 전체 데이터 소환
+for row in ws.iter_rows(min_row=3, max_row=ws.max_row, min_col=1, max_col=ws.max_column):
+    for cell in row:
+        print(cell.value, end="\t")
+    print("")
+
+print()
+print("---------------------------------------------------------")
+
+# 영어 점수가 90점 이상인 사람의 번호 추출
+for row in ws.iter_rows(min_row=4, max_row=ws.max_row, min_col=1, max_col=ws.max_column):
+    if int(row[2].value) >= 90:
+        print(f'{row[0].value}번 학생은 영어 90점 이상')
+
+# ws.insert_rows(8, 5) #8번째 행에 5줄을 추가
+# ws.delete_rows(8, 3) #8번쨰 행부터 3줄을 삭제
+# ws.delete_cols(2) #B열을 삭제
+
+print()
+print("---------------------------------------------------------")
+
+# 값 찾아 바꾸기
+for row in ws.iter_rows(max_row=4):
+    for cell in row:
+        if cell.value == "영어":
+            cell.value = "컴퓨터"
+            print(cell, "변경 완료")
+
+# 데이터 이동
+ws.move_range("B3:C53", rows=0, cols=1)
+
+
+wb.save("test.xlsx")
